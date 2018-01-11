33d2b1af034793fa3b54ee7612458a888126dac0
chenzhaolong <chenzhaolong@youzan.com>
2018-01-12 01:04:57 +0800

    feat:补充按钮组件

--- Demo/Test/app.js
+++ Demo/Test/app.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 // import {Button} from 'antd';
 import {Button} from '../../Lib/index';
+import './app.css';
 
 class Demo extends Component{
+    handle() {
+        console.log('here')
+    }
     render() {
         return (
             <div>
-                <Button type='dangers'>danger</Button>
+                <Button type='dashed' onClick={this.handle.bind(this)}>primary</Button>
             </div>
         )
     }
--- Lib/libs/button/Button.js
+++ Lib/libs/button/Button.js
@@ -1,35 +1,35 @@
 /**
  *  Button Component
  * */
-import React, {Component} from 'react';
-import {factoryStyle} from "../../sass/index";
-import {btnStyle} from '../../sass/button';
+import React, {Component, PropTypes} from 'react';
+import './button.css'
 
 export class Button extends Component {
+    static propTypes:  {
+        type: PropTypes.string,
+        className: PropTypes.string,
+        onClick: PropTypes.func
+    }
     constructor(props) {
         super(props);
         this.state = {
-            type: {
-                default: 'default',
-                primary: 'primary',
-                dashed: 'dashed',
-                danger: 'danger'
-            },
-            defaultBtnStyle: btnStyle
+            type: ['default', 'primary', 'dashed', 'danger']
         }
     }
+    _clickEventHandle(e) {
+        this.props.onClick();
+    }
     render() {
-        const defaultStyle = factoryStyle({style: this.state.defaultBtnStyle});
+        const typeStyle = this.state.type.indexOf(this.props.type) != -1 ? `dc-btn-${this.props.type}` : '';
         return(
-            <div>
                 <button
-
+                    className={typeStyle + ' ' + 'dc-btn' + ' ' + this.props.className}
+                    onClick={this._clickEventHandle.bind(this)}
                 >
                     {
                         this.props.children
                     }
                 </button>
-            </div>
         )
     }
 }
