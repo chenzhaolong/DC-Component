888d154e4cc7b345d1756446d1910cadf05ae34e
chenzhaolong_dxm <chenzhaolong@duxiaoman.com>
2018-08-11 13:06:49 +0800

    feat:初步完成notification组件50%

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -4,18 +4,34 @@ import {FaIcon} from "../fa-icon/Fa-Icon";
 import {_renderComponent, removeRootElement, createHoc, createRootElement, renderChild} from "../../../libs/tool";
 import PS from '../../../libs/ps';
 
-export class Notification extends React.Component {
-    computedHight() {
+const classNameForNofity = 'dc-notification';
 
+export class Notification extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    computedHight() {
+        const eleArr = document.getElementsByClassName(classNameForNofity);
+        if (eleArr.length === 1) {
+            return 10;
+        } else {
+            const index = eleArr.length - 2;
+            const ele = eleArr[index].firstElementChild.firstElementChild;
+            return ele.clientHeight + ele.offsetTop + 10;
+        }
     }
 
     closeNotifity() {
+
     }
 
     render() {
         const {title, message} = this.props;
 
-        const rightTop = {top: '10px', right: '10px'};
+        const top = this.computedHight();
+
+        const rightTop = {top: `${top}px`, right: '10px'};
 
         return (
             <div className='dc-notify' style={rightTop}  ref='notify'>
@@ -46,16 +62,12 @@ export class Notification extends React.Component {
     componentDidMount() {
     }
 
+
+
     /**
      * 静态方法
      */
     static notify(spec) {
-        const style = 'position: fixed; width: 330px; height: 100%; top: 0; right: 0';
-        const rootId = 'dc-notification';
-        if (!document.getElementById(rootId)) {
-            createRootElement('div', rootId, '', style);
-        }
-        const Hoc = createHoc(Notification, spec);
-        renderChild(rootId, Hoc);
+        _renderComponent('div', '', classNameForNofity)(Notification, spec);
     }
 }
